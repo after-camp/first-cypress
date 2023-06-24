1640896b9952a09927d38e5b88ade89ca249d869
Junsuk Park <park64kr63@gmail.com>
2023-06-24 11:18:38 +0900

    css selector

--- cypress/e2e/musinsa.cy.js
+++ cypress/e2e/musinsa.cy.js
@@ -1,13 +1,13 @@
 describe('musinsa', () => {
     it('should login', () => {
         cy.visit("https://www.musinsa.com/auth/login?referer=http%3A%2F%2Fwww.musinsa.com%2Fapp%2Fmypage");
-        cy.get('body > div.container.login > section.content.content--no-interval > div.login-member > form > div.login-member__form > div:nth-child(10) > div > input').type('park53kr')
-        cy.get('body > div.container.login > section.content.content--no-interval > div.login-member > form > div.login-member__form > div:nth-child(11) > div > input').type('2331456241')
+        cy.get('[name="id"]').type('park53kr')
+        cy.get('[name="pw"]').type('2331456241')
 
-        cy.get('body > div.container.login > section.content.content--no-interval > div.login-member > form > div.login-button.login-button--static > button').click();
+        cy.get('.login-form [type="submit"]').click();
     });
 
-    it("selects an option", () => {
+    it.skip("selects an option", () => {
         cy.visit("https://www.musinsa.com/app/goods/3361386?loc=goods_rank");
         cy.get('#option1').select("2")
         cy.get("#buy_option_area > div.box-btn-buy.wrap-btn-buy > div.btn_buy > a").click();
