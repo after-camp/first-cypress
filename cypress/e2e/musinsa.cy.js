3faec2109acb22160b3a136f0ea85d2959a84033
Junsuk Park <park64kr63@gmail.com>
2023-06-25 12:21:22 +0900

    expect

--- cypress/e2e/musinsa.cy.js
+++ cypress/e2e/musinsa.cy.js
@@ -1,7 +1,10 @@
 describe('musinsa', () => {
-    it('should login', () => {
+    it.skip('should login', () => {
         cy.visit("https://www.musinsa.com/auth/login?referer=http%3A%2F%2Fwww.musinsa.com%2Fapp%2Fmypage");
-        cy.get('[name="id"]').type('park53kr')
+        cy.get('[name="id"]').type('abcd')
+            .then($input => {
+                expect($input.val()).to.be.eq('morish');
+            })
         cy.get('[name="pw"]').type('2331456241')
 
         cy.get('.login-form [type="submit"]').click();
@@ -11,5 +14,12 @@ describe('musinsa', () => {
         cy.visit("https://www.musinsa.com/app/goods/3361386?loc=goods_rank");
         cy.get('#option1').select("2")
         cy.get("#buy_option_area > div.box-btn-buy.wrap-btn-buy > div.btn_buy > a").click();
+    })
+
+    it('should click an item', () => {
+        cy.visit('https://www.musinsa.com/member/find/id?referer=https%3A%2F%2Fwww.musinsa.com%2Fbrands%2Fsnowpeakapparel');
+        cy.get('[for="radioEmail"]').click()
+
+        cy.get('#inputEmai')
     })
 })
